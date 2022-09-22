baae3c97ecb5a25b641ab8a3c831641ca32a9f99
Ranecodes <ranehobasi@gmail.com>
2022-09-22 13:53:24 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import Avatar from "./components/Avatar";
-import UserProfileCard from "./components/ProfileCard";
+import React, { useState } from "react";
 
 //create root component- App component
 
 function App() {
+  const [counter, setCounter] = useState(0);
   const handleClick = (event) => console.log("Button Clicked!");
+
+  const handleIncrement = (event) => {
+    event.preventDefault();
+    setCounter((prev) => prev + 1);
+    console.log(counter);
+  };
+
+  const handleDecrement = (event) => {
+    event.preventDefault();
+    setCounter((prev) => prev - 1);
+    console.log(counter);
+  };
+
   return (
-    <section>
-      <button onClick={handleClick}>Click Me!</button>
+    <section className="counter-container">
+      <div className="counter-label">{counter}</div>
+      <button className="action-btn" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button className="action-btn" onClick={handleDecrement}>
+        Decrement
+      </button>
     </section>
   );
 }
