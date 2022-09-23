e03684d94e7a179b302e1723ec2865d6be263913
Ranecodes <ranehobasi@gmail.com>
2022-09-23 12:52:55 +0100

    Learning react props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 
 //create root component- App component
 
-function App() {
-  onsubmit = (event) => {
-    event.preventDefault();
-    const { name, email, tel } = event.target.elements;
-    alert(`Hello ${name.value}! Your email is ${email.value} and your phone number is ${tel.value}. You may proceed.` )
-  };
-
-  return (
-    <section>
-      <form className="form-container">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          name="name"
-        ></input>
-        <input type="text" placeholder="Enter your email" name="email"></input>
-        <input
-          type="tel"
-          placeholder="Enter your phone number"
-          name="tel"
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
-  );
+function Car({data}){
+  return <div className="car">{`${data.brand} ${data.color} ${data.year}`}</div>
 }
+
+function App() {
+  const cars = [
+    {
+      brand: "Tesla",
+      color: "Red",
+      year: "2022"
+    },
+    {
+      brand: "Audi",
+      color: "Black",
+      year: "2018"
+    },
+    {
+      brand: "BMW",
+      color: "Yellow",
+      year: "2010"
+    }
+  ]
+
+  const listOfCars = cars.map((car) => {
+    return <Car data={car}/>
+  })
+
+  return <section className="car-container">{listOfCars}</section>
+}
+
 export default App;
